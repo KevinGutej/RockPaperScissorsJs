7b2348b369d5a0ee16955800492a83a9012e4e21
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-04-05 22:00:18 +0200

    Comparing Choice and display

--- RockPaperScissorsJs/script.js
+++ RockPaperScissorsJs/script.js
@@ -9,3 +9,30 @@ if (computerChoice < 0.34) {
 } else {
   computerChoice = "scissors";
 }
+
+let compare = function(choice1, choice2) {
+  if (choice1 === choice2) {
+    return "The result is a tie!";
+  } else if (choice1 === "rock") {
+    if (choice2 === "scissors") {
+      return "You win! Rock beats scissors.";
+    } else {
+      return "You lose! Paper beats rock.";
+    }
+  } else if (choice1 === "paper") {
+    if (choice2 === "rock") {
+      return "You win! Paper beats rock.";
+    } else {
+      return "You lose! Scissors beats paper.";
+    }
+  } else if (choice1 === "scissors") {
+    if (choice2 === "paper") {
+      return "You win! Scissors beats paper.";
+    } else {
+      return "You lose! Rock beats scissors.";
+    }
+  }
+};
+
+// Display Result
+alert(compare(userChoice, computerChoice));
